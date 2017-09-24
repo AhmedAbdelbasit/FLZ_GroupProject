cb2b7a52617d14e80fafd42f13e58dd8727517e3
Mohamed Amin <32037981+mohameda97@users.noreply.github.com>
2017-09-24 19:32:58 +0200

    adding sun code

---
+++ Electrical/Mohamed amin/steeper/steeper.ino
@@ -0,0 +1,24 @@
+#define steppin 4 
+#define dirpin 2
+#define push 5
+#define push2 8
+#define sun 10
+#define moon 11
+
+#define CycleDelay 250
+
+int steps = 0;
+int brightsun;
+int brightmoon;
+
+
+void setup() {
+  pinMode(steppin, OUTPUT);
+  pinMode(dirpin, OUTPUT) ;
+  pinMode(push, INPUT_PULLUP);
+  pinMode(push2, INPUT_PULLUP);
+  pinMode(moon, OUTPUT);
+  pinMode(sun, OUTPUT);
+}
+
+
