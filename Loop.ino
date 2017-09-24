dff574d9e7b5b42dc8eadea33e6a82b2f646710a
Momen Fouad <32037897+MomenFouad@users.noreply.github.com>
2017-09-24 20:34:52 +0300

    adding river code

---
+++ Loop.ino
@@ -0,0 +1,38 @@
+void loop() {
+  if( digitalRead(rightbutton)==LOW ){
+    delay(250);
+    digitalWrite(dirPin,HIGH); 
+    digitalWrite(stepPin, HIGH); // first button moveforward
+    delay(10);
+    digitalWrite(stepPin, LOW);
+    delay(10);
+  }else if (digitalRead(leftbutton)==LOW){
+    delay(250);
+    digitalWrite(dirPin,LOW); 
+    digitalWrite(stepPin, HIGH);  // second button movebackword 
+    delay(10);
+    digitalWrite(stepPin, LOW);
+    delay(10);
+ }else if(digitalRead(freebutton)==LOW){
+  delay(250);
+  state=1-state ;
+         if (state==1){
+              digitalWrite(dirPin,HIGH); 
+              digitalWrite(stepPin, HIGH);
+              delay(10);
+              digitalWrite(stepPin, LOW);
+              delay(10);
+             } else {
+                 digitalWrite(stepPin, HIGH);  // hold the motor while there is no input
+                   }
+    }
+ 
+ else {
+      digitalWrite(stepPin, HIGH);  // hold the motor while there is no input 
+     
+  }
+  
+  
+     
+  }
+  
---
+++ mo2a.ino
@@ -0,0 +1,25 @@
+#define dirPin 4
+#define stepPin 3
+#define rightbutton 6 //( to move foroward )
+#define leftbutton 7  //( to move backword )
+#define freebutton 8  //( to continous moving )
+
+char Command = 'X';
+int Steps;
+int Speed;
+int state=0;
+
+const int MinSpeed=1;
+const int MaxSpeed=100;
+const int MinDelay=50;
+const int MaxDelay=3000;
+
+int DelayConstant = 25;
+
+void setup() {
+  pinMode(rightbutton,INPUT_PULLUP);
+  pinMode(leftbutton,INPUT_PULLUP);  
+  pinMode (freebutton, INPUT_PULLUP);
+}
+
+
